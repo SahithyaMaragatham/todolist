e12f3a36b882822aba3749039308bf8d90172d5b
SahithyaMaragatham <102693647+SahithyaMaragatham@users.noreply.github.com>
2024-02-15 16:53:06 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,44 +1,42 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const button = document.querySelector("button");
+const list = document.getElementById("list-container");
 
-function AddTask() {
-    if (inputBox.value === '') {
-        alert("You must write something");
-    } else {
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something!");
+    }else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+        list.appendChild(li);
+        inputBox.value = '';
         let span = document.createElement("span");
-        span.innerHTML = "\u00D7";
+        span.innerHTML = "x";
         li.appendChild(span);
     }
-    inputBox.value = ""; 
     saveData();
 }
-
-listContainer.addEventListener("click", function (e) {
-    if (e.target.nodeName === "LI") {
+list.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
-        saveData(); 
-    } else if (e.target.nodeName === "SPAN") {
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
     }
-}, false);
+});
 
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
+button.addEventListener("click", addTask);
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
 }
-
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
+function showTask(){
+    const savedData = localStorage.getItem("data");
+    if(savedData){
+        list.innerHTML = savedData;
+    }
 }
-
-function cancelTask() {
-    inputBox.value = "";
-}
-
-window.onload = function () {
-    showTask();
-};
+window.addEventListener("load", showTask);
 
